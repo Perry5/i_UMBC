7e49f0c87593b2d812787557f7fc545584af21c5
5a1t <salt@luci.fr>
2013-09-15 00:26:49 -0400

    Added verification on team data input

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,7 @@
 class Team < ActiveRecord::Base
+   validates :name, presence: true
+   validates :size, numericality: { only_integer: true, greater_than: 1}
+   validates :email, presence: true
+   validates :agreed, numericality: {only_integer: true, equal_to: 1}
+   #validates :
 end
