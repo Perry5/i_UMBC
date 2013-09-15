3a4c428384e0eb995d6a8e4bdf54fa1f622299ae
5a1t <salt@luci.fr>
2013-09-14 23:07:16 -0400

    Added basic team creation interface.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,26 @@
 class TeamsController < ApplicationController
+
+def new
+end
+
+def edit
+end
+
+def show
+end
+
+def update
+end
+
+def index
+end
+
+def create
+    render text: params[:team].inspect
+end
+
+def destroy
+end
+
+
 end
