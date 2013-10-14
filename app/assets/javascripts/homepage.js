d583757277d2b7d4573bb817a5ac607db07cc1e8
salt <the.robot.morrow@gmail.com>
2013-10-14 13:18:59 -0400

    Added new site, replacing old.

---
+++ app/assets/javascripts/homepage.js
@@ -0,0 +1,29 @@
+//  Place all the behaviors and hooks related to the matching controller here.
+//  All this logic will automatically be available in application.js.
+//  You can use CoffeeScript in this file: http://coffeescript.org/
+
+	setContent = function (display) {
+		$('#content').html($('.' + display).html());
+	};
+
+
+	setNavHandler = function () {
+		$('.nav-item').hover( function() {
+			var $this = $(this),
+			    $subnav = $this.children('.sub-nav');
+			$subnav.css('width', $this.width());
+			$subnav.toggle();
+		});
+		$('.sn-item').click( function() {
+			var $this = $(this),
+			    section_title = $this.attr('id');
+			window.location.hash = section_title;
+			setContent(section_title);
+		});
+	};
+	$('body').ready( function() {
+		setContent('home');
+		setNavHandler();
+		$('body').addClass('ready');
+	});
+	
---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,10 @@
+class HomepageController < ApplicationController
+
+	def index
+	end
+
+	def our_sponsors
+	end
+
+
+end
