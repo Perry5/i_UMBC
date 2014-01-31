6f7d1800c464716f88147c7533b56ead48ceb050
randi.c <fw48771@umbc.edu>
2014-01-30 21:36:06 -0500

    Pictures for website

--- js/home.js
+++ js/home.js
@@ -1,10 +1,10 @@
 $('body').ready( function() {
 		$('#body-content').css('opacity', '1');
+		positionFooter();
+		iframe();
 	}
 );
 
-
-positionFooter(); 
 function positionFooter() {
 	if($(document.body).height() < $(window).height()){
 		$('#footer').css({
@@ -19,5 +19,15 @@ function positionFooter() {
 					});
 				}    
 }
+
+function iframe() {
+	var photo_frame = $('#photos'),
+		ratio = 1300.0/500;
+		
+	if(photo_frame.length) {
+		photo_frame.height(ratio * photo_frame.width());
+	}
+}
+
 //disabled because I couldn't get it to work, for now.
 //$(window).bind('scroll resize click', positionFooter);
